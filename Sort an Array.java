439ec500b0a9a02e12ed4ba124b4a603e67ed9ab
AhmedYasserIbrahim <159584129+AhmedYasserIbrahim@users.noreply.github.com>
2024-07-25 04:28:36 +0300

    Create Sort an Array.java

---
+++ Sort an Array.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        if (nums.length <= 1) {
+            return nums;
+        }
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int i = left, j = mid + 1;
+        int[] temp = new int[right - left + 1];
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for (i = left; i <= right; i++) {
+            arr[i] = temp[i - left];
+        }
+    }
+}
